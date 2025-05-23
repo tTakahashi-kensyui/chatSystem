16dd38c558560b40c0992b5c606ff3bacba3736e
t-takahashi <you@example.com>
2025-05-23 16:24:04 +0900

    管理画面で削除、編集できるようになった。

--- src/main/java/com/example/demo/admin/adminController.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.admin;
-
-public class adminController {
-
-}
--- src/main/java/com/example/demo/dao/ChatDao.java
+++ src/main/java/com/example/demo/dao/ChatDao.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -32,5 +34,35 @@ public class ChatDao {
 	public void deleteById(int id) {
 	    String sql = "DELETE FROM chat WHERE id = ?";
 	    db.update(sql, id);
+	}
+
+	public void updateDb(int id, EntForm entform) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("編集の実行");
+		db.update("UPDATE chat SET name = ?, comment = ? WHERE id = ?",
+				entform.getName(),entform.getComment(),id);
+	}
+
+	public List<EntForm> selectOne(Long id) {
+		// TODO 自動生成されたメソッド・スタブ
+				//コンソールに表示
+				System.out.println("編集画面を出します");
+				//データベースから目的の1件を取り出して、そのままresultDB1に入れる
+				List<Map<String, Object>> resultDb1 = db.queryForList("SELECT * FROM chat where id=?", id);
+				//画面に表示しやすい形のList(resultDB2)を用意
+				List<EntForm> resultDb2 = new ArrayList<EntForm>();
+				//1件ずつピックアップ
+				for (Map<String, Object> result : resultDb1) {
+					//データ1件分を1つのまとまりとするので、EntForm型の「entformdb」を生成
+					EntForm entformdb = new EntForm();
+					//id、name、phoneのデータをentformdbに移す
+					entformdb.setId((int) result.get("id"));
+					entformdb.setName((String) result.get("name"));
+					entformdb.setComment((String) result.get("comment")); // ← 追加
+					//移し替えたデータを持ったentformdbを、resultDB2に入れる
+					resultDb2.add(entformdb);
+				}
+				//Controllerに渡す
+				return resultDb2;
 	}
 }
