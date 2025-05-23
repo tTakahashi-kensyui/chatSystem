9132665f34e5636993e969da3653f890ff9d18e9
t-takahashi <you@example.com>
2025-05-23 10:14:45 +0900

    chat,entformそれぞれにフィールド変数、コンストラクタ、アクセサメソッドの追加

--- src/main/java/com/example/demo/chat/Chat.java
+++ src/main/java/com/example/demo/chat/Chat.java
@@ -1,5 +1,33 @@
 package com.example.demo.chat;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class Chat {
 
+	@NotBlank(message = "名前を入力してください")
+	private String Name;
+	
+	@NotBlank(message = "文字を入力してください")
+	private String Comment;
+
+	public Chat() {
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getComment() {
+		return Comment;
+	}
+
+	public void setComment(String comment) {
+		Comment = comment;
+	}
+	
+	
 }
--- src/main/java/com/example/demo/entity/EntForm.java
+++ src/main/java/com/example/demo/entity/EntForm.java
@@ -2,4 +2,39 @@ package com.example.demo.entity;
 
 public class EntForm {
 
+	private int id;
+	
+	private String name;
+	
+	private String comment;
+
+	public EntForm() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEntName() {
+		return name;
+	}
+
+	public void setEntName(String name) {
+		this.name = name;
+	}
+
+	public String getEntComment() {
+		return comment;
+	}
+
+	public void setEntComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
 }
